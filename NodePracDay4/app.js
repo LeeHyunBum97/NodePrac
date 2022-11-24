c39283d93a8786842c8ea8f43c6105aaf2dcce47
hb9397@naver.com <hb9397@naver.com>
2022-11-24 10:44:54 +0900

    Node+MariaDB (SQL문, 샘플데이터 DB에 추가) 20221124-2

--- NodePracDay4/app.js
+++ NodePracDay4/app.js
@@ -12,6 +12,25 @@ connection.connect(function(error){
     if(error){
         console.log(error);
     } else {
-        console.log(connection);
+        // console.log(connection);
+        // 테이블 생성 구문
+        /* connection.query('create Table family(id int auto_increment primary key, name varchar(20))'); */
+
+        // 데이터 삽입 구문
+        /* connection.query('insert into family(name) values(?)', '강감찬');
+        connection.query('insert into family(name) values(?)', '을지문덕');
+        connection.query('insert into family(name) values(?)', '이순신'); */
+
+        // SELECT 구문
+        connection.query('select * from family', (err, results, fields) => {
+            if(err){
+                // 실패 했을 때.
+                console.log(err);
+                console.log("{result : false}")
+            }else{
+                let result = JSON.stringify(results);
+                console.log(result);
+            }
+        })
     }
 });
