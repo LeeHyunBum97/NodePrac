1fe2e00eefb42e317f3f2154ad43bd88b5f40b72
hb9397@naver.com <hb9397@naver.com>
2022-11-25 10:55:42 +0900

    Node+MariaDB (서버 데이터 페이지 단위로 출력하기) 20221125-1

--- NodePracDay4/Index.js
+++ NodePracDay4/Index.js
@@ -29,7 +29,7 @@ fs.existsSync(logDirctory) || fs.mkdirSync(logDirctory);
 let accessLogStream = FileStreamRotator.getStream({
     date_format: "YYYYMMDD",
     filename: path.join(logDirctory, 'access-%DATE%.log'),
-    frequency:'daily',
+    frequency: 'daily',
     verbose: false
 });
 
@@ -39,22 +39,19 @@ app.use(morgan('combined', {stream: accessLogStream}));
 // 압축해서 전송하는 옵션 설정
 app.use(compression());
 
-// POST 방식의 파라미터를 읽을 수 있도록 설정
-// GET 은 웹서버에서 처리해서 넘겨주지만 POST 방식은 본문에 담겨오고 이는 애플리케이션 서버에서 처리 되므로 따로 인코딩 처리 필요.
+// POST 방식의 파라미터를 읽을 수 있도록 설정 GET 은 웹서버에서 처리해서 넘겨주지만 POST 방식은 본문에 담겨오고 이는 애플리케이션
+// 서버에서 처리 되므로 따로 인코딩 처리 필요.
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.urlencoded({extended: true}));
 
-// Session을 DB에 저장하는 작업
-// 데이터 베이스 접속 정보
+// Session을 DB에 저장하는 작업 데이터 베이스 접속 정보
 let options = {
-    host : process.env.HOST,
-    port : process.env.MYSQLPORT,
-    user : process.env.MYSQLUSER,
-    password : process.env.PASSWORD,
-    database : process.env.DATABASE
+    host: process.env.HOST,
+    port: process.env.MYSQLPORT,
+    user: process.env.MYSQLUSER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
 }
 /* const dbConnOp = fs.readFileSync('./DBConnection.json');
 const connOp = JSON.parse(dbConnOp);
@@ -70,25 +67,24 @@ let options = {
 const MariaDBStore = require('express-mysql-session')(session);
 
 // 세션 설정
-app.use(session({
-    secret: process.env.COOKIE_SECRET,
-    resave : false,
-    saveUninitialized : true,
-    store : new MariaDBStore(options)
-}));
+app.use(
+    session({secret: process.env.COOKIE_SECRET, resave: false, saveUninitialized: true, store: new MariaDBStore(options)})
+);
 
 // 파일 업로드 설정
 const upload = multer({
-    storage : multer.diskStorage({
-        destination(req, file, done){
+    storage: multer.diskStorage({
+        destination(req, file, done) {
             done(null, 'public/img')
         },
-        filename(req, file, done){
+        filename(req, file, done) {
             const ext = path.extname(file.originalname);
             done(null, path.basename(file.originalname, ext) + Date.now() + ext);
         }
     }),
-    limits: {fileSize: 10*1024*1024}
+    limits: {
+        fileSize: 10 * 1024 * 1024
+    }
 });
 
 //정적 파일의 경로를 설정
@@ -97,12 +93,12 @@ app.use('/', express.static('public'));
 //파일 다운로드를 위한 모듈
 let util = require('util');
 let mime = require('mime');
-const { json } = require("express");
+const {json} = require("express");
 
 //데이터베이스 연결
 let connection = mysql.createConnection(options);
 connection.connect((error) => {
-    if(error){
+    if (error) {
         console.log(error);
         throw error;
     }
@@ -114,62 +110,85 @@ app.get('/', (req, res) => {
 })
 
 app.get('/item/all', (req, res) => {
-    // HTML 출력 : res.sendFile(파일경로) - 서버의 데이터 출력 못함
-    // 서버의 데이터 출력 못함 - ajax 나 fetch api를 이용해야 한다.
+    // HTML 출력 : res.sendFile(파일경로) - 서버의 데이터 출력 못함 서버의 데이터 출력 못함 - ajax 나 fetch
+    // api를 이용해야 한다. 템플릿 엔진 : res.render(파일경로, 데이터) 템플릿 엔진에 넘겨주는 데이터는 프로그래밍 언어의 데이터
+    // JSON 출력 : res.json(데이터) json 문자열의 형태로 데이터를 제공한다. FE 에서 데이터를 수신해서 출력하는 구조 2개
+    // 이상의 데이터를 조회할 때는 정렬은 필수
+    connection.query(
+        "select * from goods order by itemid desc",
+        (err, result, fields) => {
+            if (err) {
+                // 에러가 발생한 경우 에러가 발생했다고 데이터를 전송하지 않으면 안된다. 404 에러 즉, 클라이언트에서 잘못 요청한 경우를 제외하고는 BE
+                // 에서 에러를 보여주면 안된다.
+                res.json({'result': false})
+            } else {
+                // 정상 응답한 경우
+                res.json({'result': true, 'list': result});
+                //res.send(result);
+            }
 
-    // 템플릿 엔진 : res.render(파일경로, 데이터)
-    // 템플릿 엔진에 넘겨주는 데이터는 프로그래밍 언어의 데이터
-
-    // JSON 출력 : res.json(데이터)
-    // json 문자열의 형태로 데이터를 제공한다.
-    // FE 에서 데이터를 수신해서 출력하는 구조
-
-    // 2개 이상의 데이터를 조회할 때는 정렬은 필수
-    connection.query("select * from goods order by itemid desc", (err, result, fields) => {
-        if(err){
-            // 에러가 발생한 경우
-            // 에러가 발생했다고 데이터를 전송하지 않으면 안된다.
-            // 404 에러 즉, 클라이언트에서 잘못 요청한 경우를 제외하고는 BE 에서 에러를 보여주면 안된다.
-            res.json({'result':false})
-        }else{
-            // 정상 응답한 경우
-            res.json({'result':true, 'list': result});
-            //res.send(result);
         }
-        
-    })
+    )
 });
 
 // 데이터 일부분 가져오기 - URL:/item/list , 파라미터는 pageno 1개 인데 없으면 1로 설정
 app.get('/item/list', (req, res) => {
     // 파라미터 읽어오기
     let pageno = req.query.pageno;
-    if(pageno == undefined){
+
+    if (pageno == undefined) {
         pageno = 1;
     }
-    // http://localhost:9000/item/list?pageno=2 브라우저에서 강제로 매개변수 넘겨주는 방식
-    // 넘어오는지 console로 확인
-    console.log(pageno);
+    // http://localhost:9000/item/list?pageno=2 브라우저에서 강제로 매개변수 넘겨주는 방식 넘어오는지
+    // console로 확인
+    //console.log(pageno);
 
-    // item 테이블에서 itemid를 가지고 내림차순 정렬해서 페이지 단위로 데이터 가져오기
-    // select * feom item order by itemid desc limit 시작번호, 5
-    // 시작번호 = (pageno-1)*5
+    // item 테이블에서 itemid를 가지고 내림차순 정렬해서 페이지 단위로 데이터 가져오기 select * feom item order by
+    // itemid desc limit 시작번호, 5 시작번호 = (pageno-1)*5 파라미터는 무조건 문자열이므로 산술연산이 필요하다면
+    // 숫자로 형변환 해야한다. 성공여부
+    let sucOrFail = true;
+    // 성공했을 때 데이터를 저장
+    let list;
+    // 데이터 목록 가져오기
+    connection.query(
+        "select * from goods order by itemid desc limit ? , 5",
+        [(parseInt(pageno) - 1) * 5],
+        (err, result, fields) => {
+            if (err) {
+                console.log(err);
+                sucOrFail = false;
+            } else {
+                list = result;
+            }
+        }
+    );
 
-    // 파라미터는 무조건 문자열이므로 산술연산이 필요하다면 숫자로 형변환 해야한다.
-    connection.query("select * from goods order by itemid desc limit ? , 5", [parseInt((pageno-1))*5],(err, result, fields) => {
-        if(err){
+    // 테이블의 전체 데이터 개수를 가져오기
+    let dataCnt = 0;
+    connection.query("select count(*) cnt from goods;", (err, result, fields) => {
+        if (err) {
+            // 에러 발생시 실행할 구문
             console.log(err);
+        } else {
+            // 쿼리 정상 실행 되었을 때 실행할 구문
+            dataCnt = result[0].cnt;
+        }
+        // 응답 생성해서 전송
+        if (sucOrFail === false) {
             res.json({"result": false});
-        }else{
-            res.json({"result":true, "list": result});
+        } else {
+            res.json({"result": true, "list": list, "count": dataCnt});
         }
     });
+
 });
 
 // 에러 발생 시 처리하는 부분
 app.use((err, req, res, next) => {
     console.log(err);
-    res.status(500).send(err.message);
+    res
+        .status(500)
+        .send(err.message);
 });
 
 // 서버 구동
